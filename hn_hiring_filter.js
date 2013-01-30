6bf37b8082fc9364f547097db8826ef149d7dbfd
Jan Hancic <jan@popcom.si>
2013-01-30 21:17:38 +0100

    get comments text

--- hn_hiring_filter.js
+++ hn_hiring_filter.js
@@ -1,9 +1,22 @@
 (function ( document ) {
 
 	// jQuery equivalent selector: body > center > table > tbody > tr:eq(2) > td table:eq(1) > tbody > tr
-	var comments = document.querySelectorAll ( 'body > center > table > tbody > tr' )[2].querySelectorAll ( 'td table' )[1].querySelectorAll ( 'tbody > tr' );
-	// comments = comments.querySelectorAll ( 'td table' )[1];
-	// comments = comments.querySelectorAll ( 'tbody > tr' );
+	var comments = null;
+	try {
+		commentsTrs = document.querySelectorAll ( 'body > center > table > tbody > tr' )[2].querySelectorAll ( 'td table' )[1].querySelectorAll ( 'tbody > tr' );
+	} catch ( e ) {
+		alert ( "Ooops, something is wrong. Are you sure you are on a 'Ask HN: Who is hiring?' page?" );
+		return;
+	}
 
-	console.log ( comments );
+	for ( var i in commentsTrs ) {
+		var commentTr = commentsTrs[i];
+
+		try {
+			// jQuery equivalent selector: 'table td:eq(2) span.comment'
+			var commentText = commentTr.querySelectorAll ( 'table td' )[2].querySelector ( 'span.comment font' );
+			console.log ( commentText.textContent );
+		} catch ( e ) {
+		}
+	}
 } ( document ) );
