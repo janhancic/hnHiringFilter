9cdd88bfb2047f3f16e93887256c52e105cde2b5
Jan Hancic <jan@popcom.si>
2013-01-30 21:02:59 +0100

    selector for comments

--- hn_hiring_filter.js
+++ hn_hiring_filter.js
@@ -1,5 +1,9 @@
-(function () {
+(function ( document ) {
 
-	
+	// jQuery equivalent selector: body > center > table > tbody > tr:eq(2) > td table:eq(1) > tbody > tr
+	var comments = document.querySelectorAll ( 'body > center > table > tbody > tr' )[2].querySelectorAll ( 'td table' )[1].querySelectorAll ( 'tbody > tr' );
+	// comments = comments.querySelectorAll ( 'td table' )[1];
+	// comments = comments.querySelectorAll ( 'tbody > tr' );
 
-} () );
+	console.log ( comments );
+} ( document ) );
