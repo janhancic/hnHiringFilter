0850fe924b9ee2891dfd3e3b1a3dbeaec38dd279
Jan Hancic <jan@popcom.si>
2013-01-31 18:41:06 +0100

    use jQuery

--- hn_hiring_filter.js
+++ hn_hiring_filter.js
@@ -1,27 +1,35 @@
 (function ( document ) {
-	//var searchCity = 'london';
-
-	// jQuery equivalent selector: body > center > table > tbody > tr:eq(2) > td table:eq(1) > tbody > tr
-	var comments = null;
-	try {
-		commentsTrs = document.querySelectorAll ( 'body > center > table > tbody > tr' )[2].querySelectorAll ( 'td table' )[1].querySelectorAll ( 'tbody > tr' );
-	} catch ( e ) {
-		alert ( "Ooops, something is wrong. Are you sure you are on a 'Ask HN: Who is hiring?' page?" );
-		return;
-	}
-
-	for ( var i in commentsTrs ) {
-		var commentTr = commentsTrs[i];
-
-		try {
-			// jQuery equivalent selector: 'table td:eq(2) span.comment'
-			var commentText = commentTr.querySelectorAll ( 'table td' )[2].querySelector ( 'span.comment' ).textContent + '';
-
-			if ( /\b(london|boston)\b/ig.test ( commentText ) === false ) {
-				// *khm* I feel like there should be a better way to do this :)
-				commentTr.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild ( commentTr.parentNode.parentNode.parentNode.parentNode );
-			}
-		} catch ( e ) {
+	var script = document.createElement ( 'script' );
+	script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js';
+	document.body.appendChild ( script );
+	script.onload = function () {
+		var searchCities = 'london, boston';
+		searchCities = searchCities.split ( ',' );
+		for ( var i in searchCities ) {
+			searchCities[i] = searchCities[i].trim ();
 		}
+
+		$comments = $( 'body > center > table > tbody > tr:eq(2) > td table:eq(1) > tbody > tr' );
+		if ( $comments.length === 0 ) {
+			alert ( "Ooops, something is wrong. Are you sure you are on a 'Ask HN: Who is hiring?' page?" );
+			return;
+		}
+
+		$comments.each ( function () {
+			var $this = $( this );
+			var commentContent = $this.find ( 'table td:eq(2) span.comment' ).text ().toLowerCase ();
+
+			var foundCity = false;
+			for ( var i in searchCities ) {
+				if ( commentContent.indexOf ( searchCities[i] ) >= 0 ) {
+					foundCity = true;
+					break;
+				}
+			}
+
+			if ( foundCity === false ) {
+				$this.hide ();
+			}
+		} );
 	}
 } ( document ) );
