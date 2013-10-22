0779aecf5ccb8199e038a568ebf2338ee2470cff
Jan Hančič <jan.hancic@gmail.com>
2013-10-22 14:04:59 +0100

    Clean up the code

--- src/hn_hiring_filter.js
+++ src/hn_hiring_filter.js
@@ -1,48 +1,53 @@
-(function ( document ) {
-	try {
-		var commentsTrs = null;
-		try {
-			commentsTrs = document.querySelectorAll ( 'body > center > table > tbody > tr' )[2];
-			if ( document.location.pathname === '/x' ) {
-				// on a second/third/... page of thread
-				commentsTrs = commentsTrs.querySelectorAll ( 'td table tbody tr' );
-			} else {
-				// first page of thread
-				commentsTrs = commentsTrs.querySelectorAll ( 'td table' )[1].querySelectorAll ( 'tbody > tr' );
-			}
-		} catch ( e ) {
-			alert ( "Ooops, something is wrong. Are you sure you are on a 'Ask HN: Who is hiring?' page?" );
-			return;
-		}
+(function ( document, alert ) {
+	var commentsTrs = null,
+		searchCities,
+		commentTr,
+		commentText,
+		foundCity;
 
-		var searchCities = prompt ( "Enter the cities (separated by commas) you are interested in:", localStorage.getItem ( 'hn_hiring_filter' ) );
-		searchCities = searchCities.trim ();
+	try {
+		commentsTrs = document.querySelectorAll( 'body > center > table > tbody > tr' )[2];
+		if ( document.location.pathname === '/x' ) {
+			// on a second/third/... page of thread
+			commentsTrs = commentsTrs.querySelectorAll( 'td table tbody tr' );
+		} else {
+			// first page of thread
+			commentsTrs = commentsTrs.querySelectorAll( 'td table' )[1].querySelectorAll( 'tbody > tr' );
+		}
+	} catch ( e ) {
+		alert( "Ooops, something is wrong. Are you sure you are on a 'Ask HN: Who is hiring?' page?" );
+		return;
+	}
+
+	try {
+		searchCities = prompt( "Enter the cities (separated by commas) you are interested in:", localStorage.getItem( 'hn_hiring_filter' ) );
+		searchCities = searchCities.trim();
 		if ( searchCities === '' ) {
 			return;
 		}
 
-		searchCities = searchCities.toLowerCase ();
+		searchCities = searchCities.toLowerCase();
 
-		localStorage.setItem ( 'hn_hiring_filter', searchCities );
+		localStorage.setItem( 'hn_hiring_filter', searchCities );
 
-		searchCities = searchCities.split ( ',' ).map ( function ( city ) {
-			return city.trim ();
+		searchCities = searchCities.split( ',' ).map( function ( city ) {
+			return city.trim();
 		} );
 
-		for ( var i in commentsTrs ) {
-			var commentTr = commentsTrs[i];
+		for ( var commentTrIdx in commentsTrs ) {
+			commentTr = commentsTrs[commentTrIdx];
 
 			try {
-				var commentText = commentTr.innerHTML;
+				commentText = commentTr.innerHTML;
 				if ( !commentText ) {
 					continue;
 				}
 
-				commentText = commentText.toLowerCase ();
+				commentText = commentText.toLowerCase();
 
-				var foundCity = false;
+				foundCity = false;
 				for ( var i in searchCities ) {
-					if ( commentText.indexOf ( searchCities[i] ) >= 0 ) {
+					if ( commentText.indexOf( searchCities[i] ) >= 0 ) {
 
 						foundCity = true;
 						break;
@@ -50,12 +55,13 @@
 				}
 
 				if ( foundCity === false ) {
-					commentTr.parentNode.removeChild ( commentTr );
+					commentTr.parentNode.removeChild( commentTr );
 				}
 			} catch ( e ) {
+				// intentionally ignore error
 			}
 		}
 	} catch ( e ) {
-		alert ( "Something went wrong :/ please try again!" );
+		alert( "Something went wrong :/ please try again!" );
 	}
-} ( document ) );
+}( document, alert ) );
